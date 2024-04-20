c87e285eb3cbc7d9984326b477b9ae0d357ea4df
YuriiM <mokruy9999@gmail.com>
2024-04-20 16:53:08 +0200

    (task) get proxies data, PP-1

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+import {freeProxyListNet} from './parsers/free-proxy-list.net';
+
+async function main() {
+    try {
+        freeProxyListNet('https://free-proxy-list.net/').then(r => console.log('Scraping complete!'));
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+main().then(r => console.log('Script complete!'));
---
+++ src/models/proxy.ts
@@ -0,0 +1,11 @@
+export interface Proxy {
+    proxy: string;
+    port: string;
+    code: string;
+    country: string;
+    anonymity: string;
+    google: string;
+    instagram: string;
+    https: string;
+    lastChecked: string;
+}
